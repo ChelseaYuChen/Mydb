d7a4864d5f97b281cc586ef0a6305b53a4f60538
ChelseaYuChen <41324780+ChelseaYuChen@users.noreply.github.com>
2018-07-31 18:52:33 +0800

    Update MainActivity.java

--- app/src/main/java/com/byethost15/omgyimin/myapplication/MainActivity.java
+++ app/src/main/java/com/byethost15/omgyimin/myapplication/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    
+    Button btn_1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
